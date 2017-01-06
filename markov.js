518ba7453b29103c39cfccdacfb9c8ff7f803047
Ethan McTague <mctague@blinkenshell.org>
2017-01-05 19:52:51 -0500

    Separate markov library and UI source.
    This commit separates the Markov Chain handler class from the UI code,
    which allows the markov code to easily be reused as a library in other
    projects.

--- markov.js
+++ markov.js
@@ -73,25 +73,3 @@ function Markov() {
     return generated;
   }
 }
-
-// Loops to make a new chain.
-var timer = 0;
-var run = document.getElementById("run");
-var input = document.getElementById("input");
-var output = document.getElementById("output");
-var mkv, intext;
-run.onclick = function () {
-  intext = input.value.toLowerCase().split(/[\t\n\r ]+/gm);
-  //intext = input.value.toLowerCase().split('');
-  mkv = new Markov();
-  window.mkv = mkv;
-  mkv.seed(intext);
-  run.innerHTML = "Working...";
-  timer = setInterval(work, 1);
-}
-var work = function () {
-  output.value = mkv.chain(intext[Math.floor(Math.random() * intext.length)], parseInt(document.getElementById("num").value)).join(' ');
-  //output.value = mkv.chain(intext[Math.floor(Math.random() * intext.length)], parseInt(document.getElementById("num").value)).join('');
-  clearInterval(timer);
-  run.innerHTML = "Generate New Text";
-};
